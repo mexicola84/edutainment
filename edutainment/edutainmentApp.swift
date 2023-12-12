3684c25c94099509e4b6badb173bd3b0211c24bb
mexicola84 <i@jan-grimm.com>
2023-12-12 18:10:52 +0100

    Initial Commit

---
+++ edutainment/edutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  edutainmentApp.swift
+//  edutainment
+//
+//  Created by Jan Grimm on 12.12.23.
+//
+
+import SwiftUI
+
+@main
+struct edutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
